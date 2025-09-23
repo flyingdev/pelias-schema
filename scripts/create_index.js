d4a3d37af5caecd24499cc804d0a8ca23494558a
flyingdev <developinghuman1015@gmail.com>
2025-09-23 01:06:19 -0600

    proper version check for open search

--- scripts/create_index.js
+++ scripts/create_index.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const child_process = require('child_process');
 const config = require('pelias-config').generate();
 const { Client } = require('@opensearch-project/opensearch');
@@ -5,6 +6,8 @@ const cli = require('./cli');
 const schema = require('../schema');
 
 const SUPPORTED_ES_VERSIONS = '>=7.4.2';
+const SUPPORTED_OS_VERSIONS = '>=1.0.0';   // OpenSearch forked at ES 7.10.2, so treat >=1.0.0 as valid
+
 
 cli.header("create index");
 
@@ -14,30 +17,40 @@ const client = new Client({
   node: 'http://opensearch:9200'
 });
 
-// check minimum elasticsearch versions before continuing
-try {
-  child_process.execSync(`node ${__dirname}/check_version.js "${SUPPORTED_ES_VERSIONS}"`);
-} catch (e) {
-  console.error(`unsupported elasticsearch version. try: ${SUPPORTED_ES_VERSIONS}\n`);
-  process.exit(1);
-}
-
-// check mandatory plugins are installed before continuing
-try {
-  child_process.execSync(`node ${__dirname}/check_plugins.js`);
-} catch (e) {
-  console.error("please install mandatory plugins before continuing.\n");
-  process.exit(1);
-}
-
-const indexName = config.schema.indexName;
-const req = {
-  index: indexName,
-  body: schema,
-};
-
-// 👇 wrap async work in an IIFE
 (async function run() {
+  // check minimum elasticsearch versions before continuing
+  const res = await client.info();
+  const dist = _.get(res, 'body.version.distribution', 'elasticsearch');
+  if (dist === 'opensearch') {
+    try {
+      child_process.execSync(`node ${__dirname}/check_version.js "${SUPPORTED_OS_VERSIONS}"`);
+    } catch (e) {
+      console.error(`unsupported elasticsearch version. try: ${SUPPORTED_OS_VERSIONS}\n`);
+      process.exit(1);
+    }
+  } else {
+    try {
+      child_process.execSync(`node ${__dirname}/check_version.js "${SUPPORTED_ES_VERSIONS}"`);
+    } catch (e) {
+      console.error(`unsupported elasticsearch version. try: ${SUPPORTED_ES_VERSIONS}\n`);
+      process.exit(1);
+    }
+  }
+
+  // check mandatory plugins are installed before continuing
+  try {
+    child_process.execSync(`node ${__dirname}/check_plugins.js`);
+  } catch (e) {
+    console.error("please install mandatory plugins before continuing.\n");
+    process.exit(1);
+  }
+
+  const indexName = config.schema.indexName;
+  const req = {
+    index: indexName,
+    body: schema,
+  };
+
   try {
     await client.indices.create(req);
     console.log(`Index '${indexName}' created successfully.`);
