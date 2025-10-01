6821afb4c703a6ce5ae4d7042b7e4ad9b40ea043
flyingdev <developinghuman1015@gmail.com>
2025-10-01 02:33:47 -0600

    test: update testing to pass

--- test/settings.js
+++ test/settings.js
@@ -647,7 +647,7 @@ module.exports.tests.index = function(test, common) {
     var s = settings();
     t.equal(typeof s.index, 'object', 'index specified');
     t.equal(s.index.number_of_replicas, "0", 'replicas will increase index time');
-    t.equal(s.index.number_of_shards, "5", 'sharding value should use the elasticsearch default');
+    t.equal(s.index.number_of_shards, "1", 'sharding value should equal to the settings value');
     t.end();
   });
 };
